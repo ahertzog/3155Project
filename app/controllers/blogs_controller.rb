f91ae9f47d865778599f9616e234bf5434202ea9
ahertzog <ahertzog@uncc.edu>
2019-04-19 16:17:15 +0000

    fix blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,24 +1,51 @@
 class BlogsController < ApplicationController
     def index
-        @posts = Post.all
+        @blogs = Blog.all
     end
     
     def show
-        @post = Post.find(params[:id])
+        @blog = Blog.find(params[:id])
     end
     
     def new
+        @blog = Blog.new
     end
+    
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+    
+    def update
+        @blog = Blog.find(params[:id])
+        if @blog.update(blog_params)
+            redirect_to @blog
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @blog = Blog.find(params[:id])
+        @blog.destroy
+        
+        redirect_to blogs_path
+    end
+    
     
     def create
-        @post = Post.new(post_params)
-        @post.save
+        @blog = Blog.new(blog_params)
         
-        redirect_to @post
-    end
-    
-    private 
-        def post_params
-            params.require(:post).permit(:title, :body)
+        
+        if @blog.save
+            redirect_to @blog
+        else
+            render 'new'
         end
+    end
 end
+
+private 
+    def Blog_params
+            params.require(:blog).permit(:title, :body)
+    end
+
