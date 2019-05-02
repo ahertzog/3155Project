3a3320929cc01639e373b7926af2a869556bcc4e
ahertzog <ahertzog@uncc.edu>
2019-05-02 16:23:23 +0000

    Update topnav and add login button

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy'
   
   resources :sessions, only: [:create, :destroy]
+  get 'welcome/index'
   
   resources :clubs do
     resources :meetings do
