027e6537f8762fdb0911dd6bece5ee95c95c296d
lrizvano <lrizvano@uncc.edu>
2019-05-02 16:22:42 +0000

    fixed routing

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,12 +1,19 @@
 class MeetingsController < ApplicationController
+    def new
+    end
+    
+    def index
+        @meetings = Meeting.all
+    end
+    
     def create
         @club = Club.find(params[:club_id])
-        @meeting = @club.comments.create(club_params)
+        @meeting = @club.meetings.create(meeting_params)
         redirect_to club_path(@club)
     end
     
     def destroy
-        @club = club.find(params[:club_id])
+        @club = Club.find(params[:club_id])
         @meeting = @club.meetings.find(params[:id])
         @meeting.destroy
         redirect_to club_path(@club)
