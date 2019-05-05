94970915f3c16791e0735574bae27697d2d29974
lrizvano <lrizvano@uncc.edu>
2019-05-05 23:31:40 +0000

    experimented with test files

---
+++ spec/integration/member_creates_a_post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper.rb'
+
+feature"Member creates a post" do
+    scenario "Member successfully navigates to the posts page from the home page" do
+        visit welcome_index_path
+        expect(page).to have_content("NINER INTERACT")
+        click_link "Blogs"
+        expect(page).to have_content("Blog Title")
+    end
+    
+    scenario "Member successfully creates a blog" do
+        visit welcome_index_path
+        click_link "Blogs"
+        click_link "New Blog"
+        expect(page).to have_content("New Post")
+        fill_in 'blog_title', with: "This is created by RSpec"
+    end
+end
