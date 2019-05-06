e301277eaeb34029fd0a5271eba4cfeb84af3a10
ahertzog <ahertzog@uncc.edu>
2019-05-06 17:06:54 +0000

    testing updates

--- spec/integration/member_creates_a_post_spec.rb
+++ spec/integration/member_creates_a_post_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper.rb'
 
-feature"Member creates a post" do
+feature"Member creates a blog" do
     scenario "Member successfully navigates to the posts page from the home page" do
         visit welcome_index_path
         expect(page).to have_content("NINER INTERACT")
-        click_link "Blogs"
-        expect(page).to have_content("Blog Title")
+        click_link "Posts"
+        expect(page).to have_content("Listing Posts")
     end
     
-    scenario "Member successfully creates a blog" do
+    scenario "Member successfully creates a post" do
         visit welcome_index_path
-        click_link "Blogs"
-        click_link "New Blog"
-        expect(page).to have_content("New Post")
-        fill_in 'blog_title', with: "This is created by RSpec"
+        click_link "Posts"
+        click_link "Select Club to Create Post"
+        expect(page).to have_content("Listing Clubs")
+        #click on show in table
+        #fill_in 'title', with: "This is created by RSpec"
     end
 end
